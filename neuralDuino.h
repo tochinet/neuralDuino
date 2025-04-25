8e301682e0bc05de9e55400d933cfcd22dcee0d6
Tochinet <tochinet@gmail.com>
2025-04-25 14:03:29 +0200

    Update neuralDuino.h

--- neuralDuino.h
+++ neuralDuino.h
@@ -1,6 +1,9 @@
 #ifndef __neuralDuino_h__
 #define __neuralDuino_h__
 
+#define FPTYPE float // FPTYPE indicates what is used for calculations. Now still float (32-bit IEEE754)
+//#define FPTYPE Posit8
+//#define FPTYPE Posit16
 #define DEBUG 0
 #define DISPLAY_ERROR 1
 #define LEARNING_RATE 0.1
@@ -48,13 +51,12 @@ public:
 
 
 	byte inCount ; //input Nodes are only counted 
-/* counts the number of neurons that are connected to this neuron using connectInput()
-be advised this DOES NOT keep count of the inputs specified by the programmer by the setInput() function */
+	/* counts the number of neurons that are connected to this neuron using connectInput()
+	Be advised this DOES NOT keep count of the inputs specified by the programmer by the setInput() function */
 
 
 	byte numSynapse; /* keeps count of the inputs specified by setInput() and begin() 
    this counts the number of float array type inputs and not the connectInput() ones */
-
 
 	activFn activation; // associates an activation function for this neuron, user sets the address of any activation function using setActivationFn() */
 
@@ -62,14 +64,14 @@ be advised this DOES NOT keep count of the inputs specified by the programmer by
 
 	/* to prevent memory fragmentation while retaining flexibility the begin function
 	allocates memory one time only, while it is possible to do away with this
-	and have completely dynamic allocn and reallocn but it is not advisable because the RAM may get
+	and have completely dynamic alloc and realloc but it is not advisable because the RAM may get
 	fragmented
 
 	ARGs:
 	noConnections : implies that this neuron will not have any connections coming into it
-                    when noConnections = HIGH, this doesnt allocate memory for the same
-	noInputs	  : implies that this neuron will not have any float inputs coming into it from the user
-                    when noInputs = HIGH, this doesnt allocate memory for the same
+            when noConnections = HIGH, this doesnt allocate memory for connections
+	noInputs : implies that this neuron will not have any float inputs coming into it from the user
+            when noInputs = HIGH, this doesnt allocate memory for inputs
 	these are optional arguments for those people who know what they are doing  */
 
 	void begin(byte num_syn, byte noConnections = FALSE, byte noInputs = FALSE);
@@ -79,7 +81,7 @@ be advised this DOES NOT keep count of the inputs specified by the programmer by
 	void setOutput(int value); // Set the constant output value mostly for the bias node only
 	byte setDesiredOutput(float desiredOutput); // setDesiredOUtput only valid for the last nodes
 	float getOutput(); // get output from the input nodes	
-	void setActivationFn(activFn userFn); //set the activation functionfor this->node
+	void setActivationFn(activFn userFn); // set the activation function for this->node
 	void printWeights(); // print the final weights after learning has happened
 	void adjWeights(); // adjust weights according to the update rule
 	void backpropagate(); //backpropagate the betas to the connected nodes only
@@ -87,9 +89,6 @@ be advised this DOES NOT keep count of the inputs specified by the programmer by
 	void connectInput(neuron* inNode); /* connect other nodes to this->node as inputs
 	this function accepts one neuron pointer and adds to the list of input pointers
  	from which output is taken as an input for this node */
-
-	
-	
 };
 
 
